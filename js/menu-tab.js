28b70cb59974edffa2b9dd8ca6f38a899644a72d
stared329 <jamie.kim.0329@gmail.com>
2017-11-17 17:04:20 +0900

    로그인 폼 관련 영역 일부 마크업 완성, 미디어 쿼리, 메뉴 완성

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -1,21 +1,13 @@
-//문서 준비 이벤트 웹브라우저가 html 렌더링을 완료했을 때 함수를 실행한다.
-$(document).ready(function() {
-  var menu = $('.main-menu > li');
-  var subMenu = $('.sub-menu');
-  var lastSub = $('.sub-menu li:last-child a'); //.마지막 li요소
-  //main-menu 하위의 span 요소가 hover 됐을 때 
-  menu.hover(function() {
-    $(this).find('.sub-menu').toggleClass('menu-act');
-  });
-  menu.focusin(function() { //cf.focus
-    $(this).find('.sub-menu').addClass('menu-act');
-  });
-  lastSub.focusout(function() {
-    $(this).parents('.sub-menu').removeClass('menu-act');
-  });
+$(document).ready(
+  function() {
+    var menu = $('.main-menu > li');
+    menu.hover(function() {
+      $(this).find('.sub-menu').toggleClass('menu-act');
+    });
+    menu.focusin('focusin', function() {
+      $(this).siblings().find('.sub-menu').removeClass('menu-act');
+      $(this).find('.sub-menu').addClass('menu-act');
+    });
 
-  //   $(menu).hover(function() {
-  //     //this(=span)의 형제 중 sub-menu 클래스를 선택하면 menu-act 클래스를 토글시킨다.
-  //     $(this).siblings('.sub-menu').toggleClass('menu-act');
-  //   });
-});
+  }
+);
