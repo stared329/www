3f86e200ff00d877bdd062a3060767c638927357
stared329 <jamie.kim.0329@gmail.com>
2017-11-21 17:13:04 +0900

    검색 및 공지사항/자료실 마크업 및 디자인 완성

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -9,5 +9,10 @@ $(document).ready(
       $(this).find('.sub-menu').addClass('menu-act');
     });
 
+    var boardtab = $('.board h2');
+    boardtab.on('click focusin', function() {
+      // $(this).parents().find('.board-act').removeClass('board-act');
+      $(this).parent().addClass('board-act').siblings().removeClass('board-act');
+    });
   }
 );
