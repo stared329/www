9a2b9f4a73d7ab90184b1f18169f293801a9fe96
stared329 <jamie.kim.0329@gmail.com>
2017-11-23 17:04:37 +0900

    관련 사이트 포커스 이벤트 추가

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -14,5 +14,23 @@ $(document).ready(
       // $(this).parents().find('.board-act').removeClass('board-act');
       $(this).parent().addClass('board-act').siblings().removeClass('board-act');
     });
+
+    var related = $('.related-list');
+    related.on('focusin', function() {
+      $(this).addClass('list-open');
+    });
+    $('.related-list li:last-child a').on('focusout', function() {
+      $(this).parents('.related-list').removeClass('list-open');
+    });
+    // $('[role="tab"]').on('click focusin', function(e) {
+    //   e.preventDefault();
+    //   // 클릭한 tab 요소에 aria-selected=true로 지정하고 
+    //   // 형제요소중에 자신 tab 이외의 나머지 tab 요소들을 aria-selected=false로 지정한다.
+    //   $(this).attr('aria-selected', true).siblings().attr('aria-selected', false);
+
+    //   var selectedId = "#" + $(this).attr('aria-controls');
+    //   //자신은 보이게하고 다른 tabpanel은 보이지 않게 지정한다.
+    //   $(selectedId).removeClass('unvisual').siblings().addClass('unvisual');
+    // });
   }
 );
